c83efa9e7811622f918715b74c8bc95bfa8147da
AbdelrhmanRady <bedo8800@hotmail.com>
2024-02-17 23:45:21 +0200

    added spawn and despawn functions

--- car_demo/scripts/despawn_prius.py
+++ car_demo/scripts/despawn_prius.py
@@ -5,38 +5,28 @@ Script used to spawn a prius in a generic position
 """
 
 import rclpy
-from ament_index_python.packages import get_package_share_directory
-from gazebo_msgs.srv import SpawnEntity
+from gazebo_msgs.srv import DeleteEntity
 
-
-def spawn(x,y,z):
-
+def despawn():
     # Start node
     rclpy.init()
     node = rclpy.create_node("entity_spawner")
 
     node.get_logger().info(
         'Creating Service client to connect to `/spawn_entity`')
-    client = node.create_client(SpawnEntity, "/spawn_entity")
+    client = node.create_client(DeleteEntity, "/delete_entity")
 
-    node.get_logger().info("Connecting to `/spawn_entity` service...")
+    node.get_logger().info("Connecting to `/delete_entity` service...")
     if not client.service_is_ready():
         client.wait_for_service()
         node.get_logger().info("...connected!")
 
-    # Get path to the prius 
-    sdf_file_path = "../../prius_description/urdf/prius.urdf"
 
     # Set data for request
-    request = SpawnEntity.Request()
+    request = DeleteEntity.Request()
     request.name = "prius"
-    request.xml = open(sdf_file_path, 'r').read()
-    request.robot_namespace = "prius"
-    request.initial_pose.position.x = float(x)
-    request.initial_pose.position.y = float(y)
-    request.initial_pose.position.z = float(z)
 
-    node.get_logger().info("Sending service request to `/spawn_entity`")
+    node.get_logger().info("Sending service request to `/delete_entity`")
     future = client.call_async(request)
     rclpy.spin_until_future_complete(node, future)
     if future.result() is not None:
